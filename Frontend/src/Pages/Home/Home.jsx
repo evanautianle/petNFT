436a2aea87bdc7699285fcec4c2fb552f23490ac
andrecamerino <camerinoandre@gmail.com>
2025-03-20 21:25:57 +1300

    init react frontend

---
+++ Frontend/src/Pages/Home/Home.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './Home.css'
+import Navbar from '../../Components/Navbar/Navbar.jsx'
+
+const Home = () => {
+  return (
+    <div>
+        <Navbar />
+        <Navbar />
+        <Navbar />
+        <Navbar />
+        <h1>What is the name of the Evan</h1>
+    </div>
+  )
+}
+
+export default Home
